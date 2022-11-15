83a944a8197e37e51f63a378ddaf99bfab03e3f0
KenAdamsKen <d.babich@gmx.de>
2022-11-15 23:26:06 +0100

    beginning hw8

---
+++ js/menu.js
@@ -0,0 +1,35 @@
+(() => {
+    const mobileMenu = document.querySelector('.mobile-btn');
+    const openMenuBtn = document.querySelector('.icon-menu');
+    const closeMenuBtn = document.querySelector('.icon-close');
+
+    const afterBlueLinie = document.querySelectorAll('.header__link');
+
+    console.log(afterBlueLinie);
+
+    const toggleMenu = () => {
+      const isMenuOpen =
+        mobileMenu.getAttribute('aria-expanded') === 'true' || false;
+        mobileMenu.setAttribute('aria-expanded', !isMenuOpen);
+      mobileMenu.classList.toggle('is-open');
+
+
+
+      const scrollLockMethod = !isMenuOpen
+        ? 'disableBodyScroll'
+        : 'enableBodyScroll';
+      bodyScrollLock[scrollLockMethod](document.body);
+    };
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
+  
+    // Закрываем мобильное меню на более широких экранах
+    // в случае изменения ориентации устройства.
+    // Close the mobile menu on wider screens if the device orientation changes
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+      if (!e.matches) return;
+      mobileMenu.classList.remove('is-open');
+      openMenuBtn.setAttribute('aria-expanded', false);
+      bodyScrollLock.enableBodyScroll(document.body);
+    });
+  })();
